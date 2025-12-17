e7bf71f7d29768e18e7f8e698122083b40c42721
King Vagandar <poohzazazaja@gmail.com>
2025-12-18 00:48:47 +0700

    feat: test frontend preview

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
                 <Dashboard />
               </ProtectedRoute>
             }
-          />
+          /> {/* TODO: Remove this route after testing */}
           <Route
             path="/profile"
             element={
